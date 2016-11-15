786f4b58bf1fcfd03069bdb3469ef227f6dd1e1c
Li Min <li.min@rea-group.com>
2016-11-15 17:12:07 +0800

    Simple http request controller without implementation.

--- src/main/scala/dojo/scala/app/MainApp.scala
+++ src/main/scala/dojo/scala/app/MainApp.scala
@@ -1,7 +1,20 @@
 package dojo.scala.app
 
+import akka.actor.ActorSystem
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.model.{HttpResponse, HttpRequest}
+import akka.stream.ActorMaterializer
+
+import scala.concurrent.Future
+
 object MainApp {
   def main(array: Array[String]): Unit = {
-    println("hello world!")
+    implicit val actorSystem = ActorSystem()
+    implicit val executionContext = actorSystem.dispatcher
+    implicit val materializer = ActorMaterializer()
+
+    val handler: HttpRequest => Future[HttpResponse] = ???
+
+    Http().bindAndHandleAsync(handler, "localhost", 8080)
   }
 }
