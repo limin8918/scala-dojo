394d29df2ad39fb15430fc2e44ded040543b77cd
Li Min <li.min@rea-group.com>
2016-11-15 16:32:22 +0800

    add simple implementation and tests

---
+++ src/main/scala/dojo/scala/app/MainApp.scala
@@ -0,0 +1,7 @@
+package dojo.scala.app
+
+object MainApp {
+  def main(array: Array[String]): Unit = {
+    println("hello world!")
+  }
+}
---
+++ src/test/scala/dojo/scala/app/MainAppSpec.scala
@@ -0,0 +1,17 @@
+package dojo.scala.app
+
+import org.scalacheck.{Prop, Gen}
+import org.specs2.ScalaCheck
+import org.specs2.mutable.Specification
+
+object MainAppSpec extends Specification with ScalaCheck {
+  "test" should {
+    "be testable" in {
+      Prop.forAll(Gen.posNum[Int]) {
+        (i: Int) =>
+          println(s"=== $$ $i ===")
+          i must beGreaterThan(0)
+      }
+    }
+  }
+}
